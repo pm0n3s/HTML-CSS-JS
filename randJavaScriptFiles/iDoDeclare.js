2b3275d64b6046d3be6293b42d78c409d4c291a0
pm0n3s <patrick.bodin777@gmail.com>
2017-08-09 19:12:46 -0400

    working file

---
+++ randJavaScriptFiles/iDoDeclare.js
@@ -0,0 +1,20 @@
+var num1 = 42;
+var num2 = 3.14;
+var num3 = -42;
+var num4 = 4300000000
+console.log("first number is " + num1);
+console.log("second number is " + num2);
+console.log("third number is " + num3);
+console.log("fourth number is " + num4);
+var str1 = "double quotes";
+var str2 = 'single quotes';
+var str3 = 'Hello World!';
+console.log("first string is " + str1);
+console.log("second string is " + str2);
+console.log("third string is " + str3);
+var bool1 = true;
+var bool2 = false;
+console.log("first boolean is " + bool1);
+console.log("second boolean is " + bool2);
+var und = undefined;
+console.log("undefined var is " + und)

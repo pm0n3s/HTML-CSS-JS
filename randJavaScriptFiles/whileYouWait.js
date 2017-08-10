236e0d19e8672121ba7b68a264eb1644219a6a5d
pm0n3s <patrick.bodin777@gmail.com>
2017-08-09 21:15:13 -0400

    working file

---
+++ randJavaScriptFiles/whileYouWait.js
@@ -0,0 +1,34 @@
+var daysUntilMyBirthday = 60;
+while (daysUntilMyBirthday > 0) {
+    if (daysUntilMyBirthday > 30) {
+        console.log(daysUntilMyBirthday + " days until my birthday. Such a long time... :(");
+        daysUntilMyBirthday = daysUntilMyBirthday - 1;
+    }
+    else if (daysUntilMyBirthday > 5) {
+        console.log(daysUntilMyBirthday + " days until my birthday. We're getting closer!");
+        daysUntilMyBirthday = daysUntilMyBirthday - 1;
+    }
+    else {
+        console.log(daysUntilMyBirthday + " DAYS UNTIL MY BIRTHDAY!!!");
+        daysUntilMyBirthday = daysUntilMyBirthday - 1;
+    }
+}
+console.log("♪ღ♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪ღ♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•*")
+console.log("♪ღ♪░H░A░P░P░Y░ B░I░R░T░H░D░A░Y░░♪ღ♪")
+console.log("*•♪ღ♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•«")
+
+
+for (var daysUntilMyBirthday = 60; daysUntilMyBirthday > 0; daysUntilMyBirthday--) {
+    if (daysUntilMyBirthday > 30) {
+        console.log(daysUntilMyBirthday + " days until my birthday. Such a long time... :(");
+    }
+    else if (daysUntilMyBirthday > 5) {
+        console.log(daysUntilMyBirthday + " days until my birthday. We're getting closer!");
+    }
+    else {
+        console.log(daysUntilMyBirthday + " DAYS UNTIL MY BIRTHDAY!!!");
+    }
+}
+console.log("♪ღ♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪ღ♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•*")
+console.log("♪ღ♪░H░A░P░P░Y░ B░I░R░T░H░D░A░Y░░♪ღ♪")
+console.log("*•♪ღ♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•«")
